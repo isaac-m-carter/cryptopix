e722135c432a3c3bf93b9aea52d0db7464d6c8ad
isaac-m-carter <270033249@yoobeestudent.ac.nz>
2022-04-06 11:28:41 +1200

    nav

--- front/src/components/BottomNav.vue
+++ front/src/components/BottomNav.vue
@@ -1,11 +1,11 @@
 <template>
     <div class="bottom-nav-wrapper">
         <div class="bottom-nav">
-            <router-link to="/"><i @click="current_toggle" :class="{current: current_active}" class="fi fi-rr-home"></i></router-link>
-            <router-link to="/Search"><i @click="current_toggle" :class="{current: current_active}" class="fi fi-rr-search"></i></router-link>
-            <router-link to="/Listings"><i @click="current_toggle" :class="{current: current_active}" class="fi fi-rr-add"></i></router-link>
-            <router-link to="/Watchlist"><i @click="current_toggle" :class="{current: current_active}" class="fi fi-rr-heart"></i></router-link>
-            <router-link to="/Cart"><i @click="current_toggle" :class="{current: current_active}" class="fi fi-rr-shopping-cart"></i></router-link>
+            <router-link to="/"><i @click="home_toggle" :class="{current: home_active}" class="fi fi-rr-home"></i></router-link>
+            <router-link to="/Search"><i @click="search_toggle" :class="{current: search_active}" class="fi fi-rr-search"></i></router-link>
+            <router-link to="/Listings"><i @click="add_toggle" :class="{current: add_active}" class="fi fi-rr-add"></i></router-link>
+            <router-link to="/Watchlist"><i @click="favourite_toggle" :class="{current: favourite_active}" class="fi fi-rr-heart"></i></router-link>
+            <router-link to="/Cart"><i @click="cart_toggle" :class="{current: cart_active}" class="fi fi-rr-shopping-cart"></i></router-link>
         </div>
     </div>
 </template>
@@ -53,12 +53,40 @@ a{
 export default {
   data() {
         return {
-            current_active: true,
+            home_active: false,
+            search_active: false,
+            add_active: false,
+            favourite_active: false,
+            cart_active: false,
         };
     },
     methods: {
-        current_toggle() {
-            this.current_active = !this.current_active
+        home_toggle() {
+            resetAll();
+            this.home_active = !this.home_active;
+        },
+        search_toggle() {
+            resetAll();
+            this.search_active = !this.search_active;
+        },
+        add_toggle() {
+            resetAll();
+            this.add_active = !this.add_active;
+        },
+        favourite_toggle() {
+            resetAll();
+            this.favourite_active = !this.favourite_active;
+        },
+        cart_toggle() {
+            resetAll();
+            this.cart_active = !this.cart_active;
+        },
+        resetAll(){
+            this.home_active = false;
+            this.search_active = false;
+            this.add_active = false;
+            this.favourite_active = false;
+            this.cart_active = false;
         }
     },
 }  
--- front/src/views/Home.vue
+++ front/src/views/Home.vue
@@ -6,3 +6,13 @@
     <h1>home</h1>
     <router-link to="/Search">Search</router-link>
 </template>
+
+<style>
+    .home-toggle{
+    color: #3670FA;
+    background-color: #fff;
+    padding: 1.5rem;
+    border-radius: 40px;
+    margin: .4rem;
+}
+</style>
