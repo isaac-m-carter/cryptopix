3340ec3cbb34f1c1ecdfa15d368db93a1eec30d0
isaac-m-carter <270033249@yoobeestudent.ac.nz>
2022-04-05 12:11:45 +1200

    Update main.js

--- front/src/main.js
+++ front/src/main.js
@@ -1,7 +1,7 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from "./router/index";
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
 
 createApp(App)
-    .use(router)
-    .mount("#app");
+  .use(router)
+  .mount("#app");
