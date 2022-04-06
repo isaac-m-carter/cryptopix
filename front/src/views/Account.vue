169093a2657ff830d46f5b8e4961e16ea5225d6a
isaac-m-carter <270033249@yoobeestudent.ac.nz>
2022-04-07 11:38:19 +1200

    Create Account page

--- front/src/views/Account.vue
+++ front/src/views/Account.vue
@@ -1,9 +1,24 @@
+<script setup>
+    import User from '../components/account/User.vue'
+</script>
+
 <template>
     <h1>Account</h1>
+
+    <div class="main-container">
+
+        <User />
+
+    </div>
+
 </template>
 
 <style scoped>
     .fi{
         color: #3670FA;
+    }
+
+    .main-container{
+        padding: 1rem;
     }
 </style>
