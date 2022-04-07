67f281e3b0ee8f5587ecac5163344785a296cfab
isaac-m-carter <270033249@yoobeestudent.ac.nz>
2022-04-08 10:35:28 +1200

    Update on Account page
    Almost actually functioning so thats cool

--- front/src/components/account/User.vue
+++ front/src/components/account/User.vue
@@ -5,11 +5,27 @@
         </div>
 
         <div class="user-details">
+            <i class="fi fi-rr-edit" @click="editUserDetail_modal"></i>
              <h2>Details</h2>
              <h3>name</h3>
              <h3>email</h3>
              <h3>password</h3>
              <h3>ETH</h3>
+             <div class="edit-user-details" :class="{hidden: hideState}">
+                <i class="fi fi-rr-checkbox" @click="editUserDetail_modal"></i>
+                <h2>Edit</h2>
+
+                <form action="" id="edit-details" method="GET">
+
+                    <input type="text" id="user-name" name="user-name" placeholder="Name">
+                    <input type="e-mail" id="user-email" name="user-email" placeholder="Email">
+                    <input type="password" id="password" name="password" placeholder="Password">
+
+                     <div class="btn-container">
+                        <button class="submit" type="submit">Done</button>
+                     </div>
+                </form>
+             </div>
         </div>
     </div>
 
@@ -17,31 +33,14 @@
         <h2>User Portfolio</h2>
         <div class="portfolio-container account-container">
             
-            <i class="fi fi-rr-add account-item"></i>
-            <i class="fi fi-rr-add account-item"></i>
-            <i class="fi fi-rr-add account-item"></i>
-            <i class="fi fi-rr-add account-item"></i>
-            <i class="fi fi-rr-add account-item"></i>
-            <i class="fi fi-rr-add account-item"></i>
-            <i class="fi fi-rr-add account-item"></i>
-            <i class="fi fi-rr-add account-item"></i>
-            <i class="fi fi-rr-add account-item"></i>
         </div>
     </div>
 
     <div id="user-listings" class="mm">
         <h2>Listings</h2>
         <div class="listings-container account-container">
+            <router-link to="/Listings"><i class="fi fi-rr-add account-item"></i></router-link>
             
-            <i class="fi fi-rr-add account-item"></i>
-            <i class="fi fi-rr-add account-item"></i>
-            <i class="fi fi-rr-add account-item"></i>
-            <i class="fi fi-rr-add account-item"></i>
-            <i class="fi fi-rr-add account-item"></i>
-            <i class="fi fi-rr-add account-item"></i>
-            <i class="fi fi-rr-add account-item"></i>
-            <i class="fi fi-rr-add account-item"></i>
-            <i class="fi fi-rr-add account-item"></i>
         </div>
     </div>
 
@@ -49,20 +48,20 @@
         <h2>Sold Items</h2>
         <div class="sold-items-container account-container">
             
-            <i class="fi fi-rr-add account-item"></i>
-            <i class="fi fi-rr-add account-item"></i>
-            <i class="fi fi-rr-add account-item"></i>
-            <i class="fi fi-rr-add account-item"></i>
-            <i class="fi fi-rr-add account-item"></i>
-            <i class="fi fi-rr-add account-item"></i>
-            <i class="fi fi-rr-add account-item"></i>
-            <i class="fi fi-rr-add account-item"></i>
-            <i class="fi fi-rr-add account-item"></i>
         </div>
     </div>
 </template>
 
 <style scoped>
+
+
+    .edit-user-details{
+        position: absolute;
+        width: 100%;
+        height: 100%;
+        background-color: #fff;
+        top: 0;
+    }
 
     #user-profile{
         display: flex;
@@ -71,6 +70,7 @@
     }
 
     .user-details{
+        position: relative;
         width: 100%;
     }
 
@@ -101,13 +101,20 @@
         border-bottom: 2px solid #3670FA;
     }
 
-    h3{
+    h3, input{
         font-weight: 500;
         font-size: 12pt;
         margin: 0;
         padding: .25em;
         line-height: 140%;
         border-bottom: 1px solid rgba(0, 0, 0, 0.2);
+    }
+
+    input{
+        font-style: italic;
+        border: none;
+        border-bottom: 1px solid rgba(0, 0, 0, 0.2);
+        width: 10rem;
     }
 
     .mm{
@@ -119,7 +126,55 @@
         font-size: 34pt;
     }
 
+    .fi-rr-edit, .fi-rr-checkbox{
+        position: absolute;
+        right: 0;
+        font-size: 20px;
+    }
+
     i{
         display: flex;
     }
+
+    .submit{
+        text-decoration: none;
+        height: 2rem;
+        width: 7rem;
+        border-radius: 20px;
+        background: #3670FA;
+        color:white;
+        border: 1px solid transparent;
+        font-family: Montserrat;
+        font-weight: 500;
+        font-size: 16px;
+        text-align: center;
+        margin-top: .5rem;
+}
+
+    .submit:hover {
+        background: white;
+        border: 1px solid #3670FA;
+        color:#3670FA;
+}
+
+    .btn-container{
+        display: flex;
+        justify-content: center;
+    }
 </style>
+
+<script>
+    export default{
+        data() {
+            return {
+                hideState: true
+                };
+        },
+        methods: {
+            editUserDetail_modal() {
+                this.hideState = !this.hideState;
+            }
+        }
+    }
+
+</script>
