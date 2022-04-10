7ec5cdd1e96cfbf94c106bb4451cc95b875c1cbe
1001JL <85082315+1001JL@users.noreply.github.com>
2022-04-11 10:13:29 +1200

    Update Creators.vue

--- front/src/components/home/Creators.vue
+++ front/src/components/home/Creators.vue
@@ -18,6 +18,15 @@
     display: flex;
     justify-content: space-between;
 }
+.tagscontainer{
+    display: flex;
+    justify-content: space-around;
+    height: 5vh;
+    background-color: blueviolet;
+}
+.tags_item{
+    background-color: aqua;
+}
 </style>
 
 
@@ -26,9 +35,9 @@
 
 <template>
 <!-- component.vue -->
-<div v-for="NTFitem in NTFallinfo" :key="NTFitem._id">
+<div class="tagscontainer" v-for="NTFitem in NTFallinfo" :key="NTFitem._id">
     
-    <h3>{{NTFitem.tags}}</h3>
+    <h3 class="tags_item">{{NTFitem.tags}}</h3>
    
     
 </div>
@@ -62,19 +71,7 @@
 export default{
     data(){
         return{
-            fakeNTFitem: {
-                _id:'1',
-                product_name: 'NTF name', 
-                seller_id: '00', 
-                buyer_id: '00', 
-                price: 2, 
-                description:'description words', 
-                date: '', 
-                sold: false,
-                clicks:3, 
-                image: 'https://cdn.vox-cdn.com/thumbor/UBw6FphB7vxtWsbQGnmjxZgCCoA=/0x0:1252x974/1120x0/filters:focal(0x0:1252x974):format(webp):no_upscale()/cdn.vox-cdn.com/uploads/chorus_asset/file/22341171/Screen_Shot_2021_03_02_at_3.21.50_PM.png', tags: '',
-                commentmsg:'awesome!'},
-        
+           
             NTFallinfo : []
         };
     },
@@ -86,8 +83,8 @@ export default{
         }
     },
     created(){
-       // this.fetchAPI();
-       this.NTFallinfo = [this.fakeNTFitem,this.fakeNTFitem,this.fakeNTFitem,this.fakeNTFitem,this.fakeNTFitem,this.fakeNTFitem];
+       this.fetchAPI();
+     
     }
 }
 </script>
