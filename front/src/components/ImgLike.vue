06d9a8c32bb791854accc44c225cedf884f26311
YvonneYoobee <270164654@yoobeestudent.ac.nz>
2022-04-12 09:28:44 +1200

    some quick imglike changes

--- front/src/components/ImgLike.vue
+++ front/src/components/ImgLike.vue
@@ -1,21 +1,19 @@
 <template>
     <div class="mainimage">
         <img class="NFTImg" :src="NftObject.image" alt="">   
-        <div class="like"><i class="fi fi-rr-heart"></i>  </div><!-- this should toggle on and off. if on = show up in watchlist  -->
-        <div @click="liked_toggle"  :class="{liked_NFT_circle: liked}" class="like"><i :class="{liked_NFT: liked}" class="fi fi-rr-heart"></i>  </div>   
+        <!--<div class="like"><i class="fi fi-rr-heart"></i>  </div> this should toggle on and off. if on = show up in watchlist  -->
+        <!-- <div @click="liked_toggle"  :class="{liked_NFT_circle: liked}" class="like"><i :class="{liked_NFT: liked}" class="fi fi-rr-heart"></i>  </div>  -->
+
+          <div class="like" :class="{liked_NFT_circle:NftObject.like}" @click="NftObject.like = !NftObject.like">
+              <i class="fi fi-rr-heart"></i>
+           </div> 
     </div>
+
 
 </template>
 
 <style scoped>
-.liked_NFT{
-    color: red;
-}
 
-
-.likedclass{
-    color:red;
-}
 .mainimage{
     position:relative;
     margin: 0 1em;
@@ -36,7 +34,6 @@
 
 }
 
-
 .like{
     position:absolute;
     right:0;
@@ -50,8 +47,16 @@
 }
 
 .liked_NFT_circle{
-    background-color: rgba(203, 203, 203, 0.775);
+    background-color: #E0456B; /*rgba(203, 203, 203, 0.775);*/
 }
+
+/* .liked_NFT{
+    color: white;
+}
+
+.likedclass{
+    color:#E0456B;
+}*/
 
 i{
     color:white;
@@ -66,28 +71,14 @@ export default {
         return {
             my_list_array:[],
             liked: false,
-
-            inputNftNicheData:{
-                    product_name:'',
-                    seller_id:'',
-                    buyer_id:'',
-                    price:0,
-                    description:'',
-                    sold:false,
-                    clicks:0,
-                    image:'',
-                    tags:{arttag:false, gametag:false, musictag:false, photogtag:false},
-                    commentmsg:'None',
-                    like:false
-            }
         }
     },
 
     methods: {
-        liked_toggle() {
-            this.liked = !this.liked;
+        // liked_toggle() {
+        //     this.liked = !this.liked;
             
-        }
+        // }
     }
     
 }
