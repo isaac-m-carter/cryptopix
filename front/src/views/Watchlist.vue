60297f7c84ebf4465397528ef6c77f197f643fd4
1001JL <85082315+1001JL@users.noreply.github.com>
2022-04-12 11:03:59 +1200

    Update Watchlist.vue

--- front/src/views/Watchlist.vue
+++ front/src/views/Watchlist.vue
@@ -1,7 +1,17 @@
+<style scoped>
+.W_img{
+    height: 30vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+}
+</style>
 <template>
     <h1>Watchlist</h1>
     <div v-for="NTFitem in NTFallinfo" :key="NTFitem._id">  
-    <div class="liked">{{NTFitem.cart}}</div>  
+    <button @click="variable = !variable">
+    <img :src="NTFitem.image" class="W_img">
+    </button>
 </div>
 </template>
 
@@ -11,13 +21,13 @@ export default{
     data(){
         return{
            
-        
+            variable : false,
             NTFallinfo : []
         };
     },
     methods:{
         async fetchAPI(){
-            const response = await fetch('http://localhost:4000/users/');
+            const response = await fetch('http://localhost:4000/nftniches/');
             const fetchedData = await response.json();
             this.NTFallinfo= fetchedData;
         }
