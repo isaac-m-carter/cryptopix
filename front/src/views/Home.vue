95b6dce6a94cd3f73a44fb70494ce4775af1dc64
1001JL <85082315+1001JL@users.noreply.github.com>
2022-04-13 11:43:18 +1200

    Update Home.vue

--- front/src/views/Home.vue
+++ front/src/views/Home.vue
@@ -1,15 +1,37 @@
 <script setup>
 import CreatorsVue from '../components/home/Creators.vue';
 import FilterVue from '../components/home/Filter.vue';
+</script>
 
-
+<script>
+export default {
+    data(){
+        return{
+            filterTags:{
+                allItems:false,
+                artFiltered:false,
+                gameFiltered:false,
+                photoFiltered:false,
+                musicFiltered:false
+            }
+        }
+    }
+}
 </script>
 
 <template>
     
     <h3>HOME</h3>
-    <FilterVue />
-    <CreatorsVue />
+        
+    <div class="wrapper-tags"> 
+        <p class="tags_item" :class="{tag_selected: filterTags.allItems}" @click="filterTags.allItems= !filterTags.allItems">All Items</p>
+        <p class="tags_item" :class="{tag_selected: filterTags.artFiltered}" @click="filterTags.artFiltered= !filterTags.artFiltered">Art</p>
+        <p class="tags_item" :class="{tag_selected: filterTags.gameFiltered}" @click="filterTags.gameFiltered= !filterTags.gameFiltered">Game</p>
+        <p class="tags_item" :class="{tag_selected: filterTags.photoFiltered}" @click="filterTags.photoFiltered= !filterTags.photoFiltered">Photography</p>
+        <p class="tags_item" :class="{tag_selected: filterTags.musicFiltered}" @click="filterTags.musicFiltered= !filterTags.musicFiltered">Musician</p>
+    </div>
+
+    <!-- <CreatorsVue /> -->
 </template>
 
 <style>
@@ -19,5 +41,19 @@ import FilterVue from '../components/home/Filter.vue';
     padding: 1.5rem;
     border-radius: 40px;
     margin: .4rem;
+}
+.tags_item{
+    background-color: #515661;
+    border-radius: 14px;
+   padding: 6px;
+   color: white;
+    
+}
+.tag_selected{
+    background-color: #3772FF;
+}
+.wrapper-tags{
+    display: flex;
+    justify-content: space-between;
 }
 </style>
